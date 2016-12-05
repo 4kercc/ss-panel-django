e44f3b0974404d6f6319671ad75af4512c764a65
DBLobster <db.lobster@gmail.com>
2016-12-05 21:57:47 +0800

    fix: require_safe no '()'.

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.http import require_safe
 
 
-@require_safe()
+@require_safe
 def index(request):
     c = {
         'title': '首页',
@@ -10,7 +10,7 @@ def index(request):
     return render_to_response(request, 'panel/index.html', c)
 
 
-@require_safe()
+@require_safe
 def status(request):
     c = {
         'title': '资金状态',

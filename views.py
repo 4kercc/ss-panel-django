d75ba8da098622cc29c98d8d0b0a245b35f5a05d
DBLobster <db.lobster@gmail.com>
2016-12-05 21:50:18 +0800

    new pages: index, status.
    在工程的 urls.py 内的 urlpatterns 里增加以下语句:
        url(r'^panel/', include('panel.urls')),

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+@require_safe()
+def index(request):
+    c = {
+        'title': '首页',
+    }
+    return render_to_response(request, 'panel/index.html', c)
+
+
+@require_safe()
+def status(request):
+    c = {
+        'title': '资金状态',
+    }
+    return render_to_response(request, 'panel/index.html', c)
